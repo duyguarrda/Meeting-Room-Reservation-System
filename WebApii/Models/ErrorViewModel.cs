2638020dae993daaeab64d90f8f95e9c2a21a117
Duygu <72448829+duyguarrda@users.noreply.github.com>
2023-08-11 15:41:39 +0300

    Delete WebApii directory

--- WebApii/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApii.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
